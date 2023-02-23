21ce307ff773583d69b91cd28cbe1118ed44abee
Linbin Pang <i@teenet.me>
2023-02-22 20:22:40 -0600

    Port Change back to 3000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const plus = require('./plus')
 const cors = require('cors')
 const app = express()
-const port = 3001
+const port = 3000
 
 app.use(cors({
   origin: '*'
